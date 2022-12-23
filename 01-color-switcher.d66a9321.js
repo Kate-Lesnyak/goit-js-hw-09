cb64da2e96b0f180cc3320ebf277b561ce168520
Kate-Lesnyak <100161294+Kate-Lesnyak@users.noreply.github.com>
2022-12-23 13:47:04 +0000

    Deploying to gh-pages from @ Kate-Lesnyak/goit-js-hw-09@d4cf3ac074c5e397749ccfacfbb121ad85059e15 🚀

---
+++ 01-color-switcher.d66a9321.js
@@ -0,0 +1,2 @@
+!function(){var t=document.body,e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");e.addEventListener("click",(function(){d=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),e.disabled=!0,a.disabled=!1})),a.addEventListener("click",(function(){clearInterval(d),e.disabled=!1,a.disabled=!0}));var d=null;a.disabled=!0}();
+//# sourceMappingURL=01-color-switcher.d66a9321.js.map
